d337af191b869c93b02ecc0b322e82027703b831
Mandi <haase1020@gmail.com>
2021-01-14 08:23:51 -0500

    add helmet

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+app.use(helmet());
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
