c77e6cdf8db68533a459f2e91f629b1936d8ee73
Mandi <haase1020@gmail.com>
2021-01-15 07:29:50 -0500

    finish lecture but having helmet issues

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-const helmet = require("helmet");
-app.use(helmet());
+// const helmet = require("helmet");
+// app.use(helmet());
 
 app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 app.post("/ajax", (req, res) => {
   console.log(req.body);
-  res.send("test");
+  res.json(["test", 1, 2, 3, 4]);
 });
 
 app.listen(3000);
